8c5ae7053a57fb71be5bccce2c27227ca11bdb99
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-31 21:36:43 +0000

    Updated homepage.jsx

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+  <div>
+    <p>Welcome! You must be here to view Balloonicorn's Trading Cards!</p>
+    <p>Click <a href="/cards">here</a> to view all cards.</p>
+    <br /><img id="welcome-img" src="/static/img/balloonicorn.jpg" />
+  </div>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
